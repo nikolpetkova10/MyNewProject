cd29224ba86ef5a331d6d26a7e4d5682559d4edd
nikolpetkova10 <petkovaniki123@gmail.com>
2025-12-18 13:06:00 +0200

    Fix variable assignment for age input

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ st.title("Моето първо Streamlit приложение")
 name = st.text_input("Как се казваш?")
 if name:
   st.write(f"Здравей, {name}!")
-name = st.text_input("На колко години си?")
- if name:
+age = st.text_input("На колко години си?")
+ if age:
    st.write(f"Интересно!")
   

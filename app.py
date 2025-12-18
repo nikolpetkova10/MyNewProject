e39bad66488945d016d66dafa81706e470cc4d43
nikolpetkova10 <petkovaniki123@gmail.com>
2025-12-18 13:05:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,7 @@ st.title("Моето първо Streamlit приложение")
 name = st.text_input("Как се казваш?")
 if name:
   st.write(f"Здравей, {name}!")
+name = st.text_input("На колко години си?")
+ if name:
+   st.write(f"Интересно!")
   

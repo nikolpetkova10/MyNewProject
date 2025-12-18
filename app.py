ed6357d6e002f4ecade1ef3df7c77f414178a16c
nikolpetkova10 <petkovaniki123@gmail.com>
2025-12-18 13:07:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ name = st.text_input("Как се казваш?")
 if name:
   st.write(f"Здравей, {name}!")
 age = st.text_input("На колко години си?")
- if age:
-   st.write(f"Интересно!")
+if age:
+   st.write(f"Забавно е да си на {age}!")
   
